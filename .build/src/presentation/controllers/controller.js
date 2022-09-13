2a6ab8106dc947e84bdb14f3ec567ebea7cbab99
João Seixas <joaoseixas88@gmail.com>
2022-09-13 11:19:56 -0300

    fix: remove .build from branch

--- .build/src/presentation/controllers/controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export class Controller {
-// 	async handle(params: HttpRequest): Promise<HttpResponse>{}
-// }
-//# sourceMappingURL=controller.js.map
