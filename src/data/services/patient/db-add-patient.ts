ba069683de1de4fe2047827cb518dd6f6e922134
João Seixas <joaoseixas88@gmail.com>
2022-09-20 07:22:10 -0300

    refac: adjust schedule order

--- src/data/services/patient/db-add-patient.ts
+++ src/data/services/patient/db-add-patient.ts
@@ -1,5 +1,6 @@
 import { UuidGenerator } from "@/data/contracts/cryptography";
 import { AddPatient } from "@/domain/features/patient/add-patient";
+import { WeekDay } from "@/domain/models";
 import { AddPatientRepository } from "../../contracts/repos/patient-repository";
 
 export class DbAddPatient implements AddPatient {
@@ -9,13 +10,14 @@ export class DbAddPatient implements AddPatient {
 	) {}
 	async add(params: AddPatient.Params): Promise<boolean> {
 		const { age, name, price, weekDays, userId, homecareId } = params;
+		const formattedWeekDays = weekDays.map((day: any) => day.toLowerCase());
 
 		const id = this.uuidGenerator.generate();
 		const patient: AddPatientRepository.Params = {
 			age,
 			name,
 			price,
-			weekDays,
+			weekDays: formattedWeekDays,
 			id,
 			userId,
 			homecareId,
--- src/data/services/schedule/mount-schedule.ts
+++ src/data/services/schedule/mount-schedule.ts
@@ -6,23 +6,25 @@ export class MountScheduleService implements MountSchedule {
 	constructor(private readonly getPatientsService: GetPatients) {}
 	async mount(params: MountSchedule.Params): Promise<Schedule> {
 		const patients = await this.getPatientsService.get(params);
-		const schedule: Schedule = patients.reduce((acc, patient) => {
-			
-			for (let day of patient.weekDays) {
-				console.log(acc)
-				console.log(day)
-				acc[day].push(patient.name);
-			}
-			return {...acc}
-		}, {
-			friday: [],
-			monday: [],
-			saturday: [],
-			sunday: [],
-			thursday: [],
-			tuesday: [],
-			wednesday: []
-		} as Schedule);
-		return schedule
+		const schedule: Schedule = patients.reduce(
+			(acc, patient) => {
+				for (let day of patient.weekDays) {
+					console.log(acc);
+					console.log(day);
+					acc[day].push(patient.name);
+				}
+				return { ...acc };
+			},
+			{
+				sunday: [],
+				monday: [],
+				tuesday: [],
+				wednesday: [],
+				thursday: [],
+				friday: [],
+				saturday: [],
+			} as Schedule
+		);
+		return schedule;
 	}
 }
